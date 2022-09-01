0c7c3d64fa05f4a70c4a0d57c4dbeafb6fce36b6
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2022-09-01 23:01:57 +0530

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  }
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmitHandler}>
+        <div className='form-control'>
+          <label htmlFor='name'>Search your favorite Cocktail</label>
+          <input type='text' id='name' ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Sint sint minim minim nisi labore est occaecat aliqua magna nisi qui laboris. Ullamco ad in enim laborum duis proident do veniam exercitation. Proident est aliquip officia consectetur pariatur ullamco duis nulla. Non adipisicing qui nisi deserunt ut et dolore ut.
+
+        Culpa labore id ex aliqua fugiat minim. Incididunt et duis enim elit dolor proident et ullamco nulla laboris laborum. Esse nulla duis sit non quis nostrud exercitation ipsum consequat. Et sit id elit ullamco id.
+      </p>
+    </section>
+  )
+}
+
+export default About
